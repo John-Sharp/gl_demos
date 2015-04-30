1830e6924c226d5f00f036761024ce1f422ffc7b
John Sharp <johnjonnosharp@gmail.com>
2015-04-30 07:13:45 +0100

    Incorporated texture

--- menge_sponge/menge_sponge.cpp
+++ menge_sponge/menge_sponge.cpp
@@ -6,6 +6,7 @@
 #include <glm/glm.hpp>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
+#include <SDL2/SDL_image.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "../simple_obj_reader/bin_obj_reader.hpp"
@@ -54,6 +55,36 @@ void init_sdl_gl_window(int w, int h, const char*title)
     glEnable(GL_CULL_FACE);
     // Accept fragment if it closer to the camera than the former one
     glDepthFunc(GL_LESS); 
+}
+
+GLuint load_texture(const char *fname)
+{
+    GLuint texture_id;
+
+    SDL_Surface *img = IMG_Load(fname);
+
+    if(!img){
+        fprintf(stderr, "Error! Could not load %s\n", fname);
+        exit(1);
+    }
+
+    glGenTextures(1, &texture_id);
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img->w, img->h,
+            0, GL_RGB, GL_UNSIGNED_BYTE, img->pixels);
+
+    SDL_FreeSurface(img);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(
+        GL_TEXTURE_2D,
+        GL_TEXTURE_MAG_FILTER,
+        GL_LINEAR_MIPMAP_LINEAR);
+
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    return texture_id;
 }
 
 void destroy_sdl_gl_window()
@@ -122,6 +153,11 @@ int main()
     GLint uv_attrib = glGetAttribLocation(shader_program, "uv");
     glVertexAttribPointer(uv_attrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(uv_attrib);
+
+    GLuint texture_id = load_texture("resources/cube_texture.png");
+    GLuint sampler_id = glGetUniformLocation(
+        shader_program,
+        "texture_sampler");
 
 
     // Get handle for our "MVP" uniform

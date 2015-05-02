6ba4c16a9f51bc62f56532d0fa1c48dd6b1c50b1
John Sharp <johnjonnosharp@gmail.com>
2015-05-02 16:34:50 +0100

    Got basic shading model working

--- menge_sponge/menge_sponge.cpp
+++ menge_sponge/menge_sponge.cpp
@@ -13,6 +13,8 @@
 #include "../shader_compiler/shader_compiler.hpp"
 #include "../InputProcessor/InputProcessor.hpp"
 
+#define DEBUG_MODE
+
 SDL_GLContext main_context;
 SDL_Window *window;
 
@@ -23,8 +25,10 @@ void init_sdl_gl_window(int w, int h, const char*title)
         exit(1);
     }
 
+#ifndef DEBUG_MODE
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+#endif
 
     window = SDL_CreateWindow(
             title,
@@ -119,8 +123,8 @@ int main()
         normals);
 
     GLuint shader_program = compile_shader(
-        "../shaders/simple.vertexshader",
-        "../shaders/simple.fragmentshader");
+        "../shaders/basic_shading.vertexshader",
+        "../shaders/basic_shading.fragmentshader");
     glUseProgram(shader_program);
 
     GLuint element_bo;
@@ -132,7 +136,7 @@ int main()
         &element_array[0],
         GL_STATIC_DRAW);
 
-    GLuint vertex_bo, uv_bo;//, normal_bo;
+    GLuint vertex_bo, uv_bo, normal_bo;
 
     glGenBuffers(1, &vertex_bo);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_bo);
@@ -142,7 +146,7 @@ int main()
         &vertices[0],
         GL_STATIC_DRAW);
 
-    GLint pos_attrib = glGetAttribLocation(shader_program, "position");
+    GLint pos_attrib = glGetAttribLocation(shader_program, "position_modelspace");
     glVertexAttribPointer(pos_attrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(pos_attrib);
 
@@ -157,28 +161,43 @@ int main()
     glVertexAttribPointer(uv_attrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(uv_attrib);
 
+    glGenBuffers(1, &normal_bo);
+    glBindBuffer(GL_ARRAY_BUFFER, normal_bo);
+    glBufferData(
+            GL_ARRAY_BUFFER,
+            normals.size() * sizeof(normals[0]),
+            &normals[0],
+            GL_STATIC_DRAW);
+    GLint normal_attrib = glGetAttribLocation(shader_program, "normal_modelspace");
+    glVertexAttribPointer(normal_attrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(normal_attrib);
+
     GLuint texture_id = load_texture("resources/cube_texture.png");
-    GLuint sampler_id = glGetUniformLocation(
-        shader_program,
-        "texture_sampler");
+    // GLuint sampler_id = glGetUniformLocation(
+    //     shader_program,
+    //     "texture_sampler");
 
-
-    // Get handle for our "MVP" uniform
-    GLuint MVP_id = glGetUniformLocation(shader_program, "MVP");
 
     InputProcessor in_processor(0.001, 0.001, glm::vec3(0.0, 0.0, 4));
 
     glm::mat4 model = glm::mat4(1.0f);
     glm::mat4 view = in_processor.get_view_mat();
     glm::mat4 projector = in_processor.get_proj_mat();
-    glm::mat4 MVP = projector * view * model;
 
-    glUniformMatrix4fv(MVP_id, 1, GL_FALSE, &MVP[0][0]);
+    glm::vec3 light_position_worldspace = glm::vec3(2.0, 2.0, 2.0);
+    GLuint light_pos_id = glGetUniformLocation(shader_program, "light_position_cameraspace");
+    glm::vec3 light_position_cameraspace(view * glm::vec4(light_position_worldspace, 1.0));
+
+    GLuint MV_id = glGetUniformLocation(shader_program, "MV");
+    glm::mat4 MV = view * model;
+
+    GLuint MVP_id = glGetUniformLocation(shader_program, "MVP");
+    glm::mat4 MVP = projector * view * model;
 
     // Bind our texture in Texture Unit 0
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture_id);
-    // Set our "myTextureSampler" sampler to user Texture Unit 0
+    // Set our sampler to user Texture Unit 0
     glUniform1i(texture_id, 0);
 
     Uint32 curr_time = SDL_GetTicks();
@@ -193,10 +212,51 @@ int main()
         view = in_processor.get_view_mat();
         projector = in_processor.get_proj_mat();
         MVP = projector * view * model;
-
         glUniformMatrix4fv(MVP_id, 1, GL_FALSE, &MVP[0][0]);
 
+        MV = view * model;
+        glUniformMatrix4fv(MV_id, 1, GL_FALSE, &MV[0][0]);
+
+        glm::vec3 light_position_cameraspace(view * glm::vec4(light_position_worldspace, 1.0));
+        glUniform3f(light_pos_id, light_position_cameraspace.x, light_position_cameraspace.y, light_position_cameraspace.z);
+
         glDrawElements(GL_TRIANGLES, element_array.size(), GL_UNSIGNED_INT, 0);
+
+#ifdef DEBUG_MODE
+        glUseProgram(0);
+
+        // Draw normals
+        glMatrixMode(GL_PROJECTION);
+        glLoadMatrixf((const GLfloat*)&projector[0]);
+        glMatrixMode(GL_MODELVIEW);
+        glm::mat4 MV = view * model;
+        glLoadMatrixf((const GLfloat*)&MV[0]);
+
+        glColor3f(0,0,1);
+        glBegin(GL_LINES);
+        for (unsigned int i=0; i<element_array.size(); i++){
+            glm::vec3 p = vertices[element_array[i]];
+            glVertex3fv(&p.x);
+            glm::vec3 o = glm::normalize(normals[element_array[i]]);
+            p+=o*0.1f;
+            glVertex3fv(&p.x);
+        }
+        glEnd();
+
+        // Draw line pointing to light position
+        glMatrixMode(GL_MODELVIEW);
+        glLoadMatrixf((const GLfloat*)&view);
+        glColor3f(1,0,0);
+        glBegin(GL_LINES);
+
+        glm::vec3 p = light_position_worldspace;
+        glVertex3fv(&p.x);
+        p = glm::vec3(0, 0, 0);
+        glVertex3fv(&p.x);
+        glEnd();
+
+        glUseProgram(shader_program);
+#endif
         SDL_GL_SwapWindow(window);
 
         while (SDL_PollEvent(&event)) {
